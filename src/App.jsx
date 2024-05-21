0a67361c8423075eb673d2b4b13583ac962b8119
JuShapiro <yuliashepel19@gmail.com>
2024-05-21 11:36:21 +0200

    LangSwitcher

--- src/App.jsx
+++ src/App.jsx
@@ -2,66 +2,114 @@ import "modern-normalize";
 import { useId, useState } from "react";
 import "./App.css";
 
-const LoginForm = ({ onLogin }) => {
-  const loginId = useId();
-  const passwordId = useId();
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const form = event.target;
-    const { login, password } = form.elements;
-    onLogin({
-      login: login.value,
-      password: password.value,
-    });
+// const LoginForm = ({ onLogin }) => {
+//   const loginId = useId();
+//   const passwordId = useId();
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     const form = event.target;
+//     const { login, password } = form.elements;
+//     onLogin({
+//       login: login.value,
+//       password: password.value,
+//     });
 
-    form.reset();
-  };
-  return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor={loginId}>Login</label>
-      <input type="text" name="login" id={loginId} />
+//     form.reset();
+//   };
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <label htmlFor={loginId}>Login</label>
+//       <input type="text" name="login" id={loginId} />
 
-      <label htmlFor={passwordId}>Password</label>
-      <input type="password" name="password" id={passwordId} />
+//       <label htmlFor={passwordId}>Password</label>
+//       <input type="password" name="password" id={passwordId} />
 
-      <button type="submit">Login</button>
-    </form>
-  );
-};
-const MyComponent = () => {
-  const id = useId();
+//       <button type="submit">Login</button>
+//     </form>
+//   );
+// };
+// const MyComponent = () => {
+//   const id = useId();
+//   return (
+//     <div>
+//       <label htmlFor={id}>Text field label</label>
+//       <input type="text" id={id} />
+//     </div>
+//   );
+// };
+// const SearchBar = () => {
+//   const [inputValue, setInputValue] = useState("");
+
+//   const handleChange = (evt) => {
+//     setInputValue(evt.target.value);
+//   };
+//   return (
+//     <div>
+//       <input type="text" value={inputValue} onChange={handleChange} />
+//       <p>{inputValue}</p>
+//     </div>
+//   );
+// };
+
+// const App = () => {
+//   const handleLogin = (userData) => {
+//     console.log(userData);
+//   };
+//   return (
+//     <div>
+//       <h1>Please login to your account!</h1>
+//       <LoginForm onLogin={handleLogin} />
+//       <MyComponent />
+
+//       <SearchBar />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+// const LangSwitcher = () => {
+//   const selectId = useId();
+
+//   return (
+//     <div>
+//       <label htmlFor={selectId}>Choose language</label>
+//       <select id={selectId}>
+//         <option value="en">English</option>
+//         <option value="uk">Ukrainian</option>
+//         <option value="pl">Polish</option>
+//       </select>
+//     </div>
+//   );
+// };
+
+// export default LangSwitcher;
+
+const LangSwitcher = ({ value, onSelect }) => {
+  const selectId = useId();
+
   return (
     <div>
-      <label htmlFor={id}>Text field label</label>
-      <input type="text" id={id} />
-    </div>
-  );
-};
-const SearchBar = () => {
-  const [inputValue, setInputValue] = useState("");
-
-  const handleChange = (evt) => {
-    setInputValue(evt.target.value);
-  };
-  return (
-    <div>
-      <input type="text" value={inputValue} onChange={handleChange} />
-      <p>{inputValue}</p>
+      <label htmlFor={selectId}>Choose language</label>
+      <select
+        id={selectId}
+        value={value}
+        onChange={(evt) => onSelect(evt.target.ariaValueText)}
+      >
+        <option value="uk">Ukrainian</option>
+        <option value="en">English</option>
+        <option value="pl">Polish</option>
+      </select>
     </div>
   );
 };
 
 const App = () => {
-  const handleLogin = (userData) => {
-    console.log(userData);
-  };
+  const [lang, setLang] = useState("uk");
   return (
     <div>
-      <h1>Please login to your account!</h1>
-      <LoginForm onLogin={handleLogin} />
-      <MyComponent />
-
-      <SearchBar />
+      <p>Selected language: {lang}</p>
+      <LangSwitcher value={lang} onSelect={setLang} />
     </div>
   );
 };
